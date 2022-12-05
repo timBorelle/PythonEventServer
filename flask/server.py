85ee028f7b38a2150865d6d42e4eefb0d51cc335
timBorelle <tim-borelle@protonmail.com>
2022-12-05 17:13:07 +0100

    add 2 API routes : /list_events and /remove_events

--- flask/server.py
+++ flask/server.py
@@ -7,6 +7,9 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 client = MongoClient("mongo:27017")
+db = client["local"]
+col = db["events"]
+
 
 # Add event
 # args : start, stop (optional) and tags
@@ -21,15 +24,38 @@ def add_event():
         return jsonify({'success': False, 'message': 'Your event can\'t be added. It needs tags argument.'}) 
     elif tags:
         tags = tags.split(",")
-
     data = {"start": start, "stop": stop, "tags": tags}
-    db = client["local"]
-    col = db["events"]
     try:
         x = col.insert_one(data)
     except:
         return jsonify({'success': False, 'message': 'Your event can\'t be inserted into MongoDB.'})  
     return jsonify({'success': True, 'message': 'Your event has been added', 'event': str(data)})
+
+# List events
+# no args
+@app.route('/list_events', methods = ['GET'])
+def list_events():
+    data = []
+    try:
+        for x in col.find():
+            data.append(str(x))
+        return jsonify({'success': True, 'data': data})
+    except:
+        return jsonify({'success': False, 'message': 'You can\'t get all events from MongoDB.'})
+
+# Remove all events
+# no args
+@app.route('/remove_events', methods = ['GET'])
+def remove_events():
+    try:
+        x = col.delete_many({})
+    except:
+        return jsonify({'success': False, 'message': 'Failed to delete all documents inside events MongoDB collection.'})
+    if x.deleted_count == 0:
+        SuccessResult = False
+    else:
+        SuccessResult = True
+    return jsonify({'success': SuccessResult, 'message': str(x.deleted_count)+" documents deleted."})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
